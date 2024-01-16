d5caefd0c0327311d080f631f74b13506e4be730
jaehee <y2hjjh@naver.com>
2024-01-16 13:45:55 +0900

    Initial Commit

---
+++ src/main/java/spring/podocodo/PodocodoApplication.java
@@ -0,0 +1,13 @@
+package spring.podocodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodocodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodocodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/podocodo/PodocodoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.podocodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodocodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
